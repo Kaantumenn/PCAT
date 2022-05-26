c13a83f4d321c2ba01b1b6d7f41a2658ca1e010c
Mahmut Kaan Tümen <mtuemen@Mahmuts-MacBook-Pro.local>
2022-05-26 15:57:59 +0300

    Static files uploaded.

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const fn = function () {
-  console.log('Hello World!');
-};
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static('public'));
 
-fn();
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
